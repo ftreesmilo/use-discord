ea4848c08688bec2ef16be86a8028101ab5ce0c0
ftreesmilo <58193105+ftreesmilo@users.noreply.github.com>
2022-05-15 11:28:27 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# use-discord
+A React context hook to log in using a Discord identity without a server-side
+component.
+
+The code will refresh login and role status every hour. I use a service worker
+to handle the redirect_uri.  I'll work on getting that added as something you
+can import and stick in your fetch handler.
+
+## Installation
+```bash
+npm i use-discord
+```
+
+## Use
+In your app's root somewhere:
+```jsx
+    <DiscordProvider
+      client_id={'969261945310031913'}
+      guilds={['783410937902006374', '551763693378076683']}
+    >
+      <TheRestOfYourApp />
+    </DiscordProvider>
+```
+
+### Provider options:
+
+#### client_id
+The client ID you get when
+[registering your "app" with discord.](https://discord.com/developers/applications)
+
+#### guilds
+An array of strings that represent all the guilds you wish to look for roles on.
+
+#### scopes
+An array of strings that represent the scopes that this hook will request.
+Default: `['identify', 'guilds.members.read']`
+
+#### redirect_uri
+Where discord will send the user during authorization flow.
+Default: `${location.origin}/discord`
+
+
+In a component hook:
+```js
+const MyComponent = () => {
+  const discord = useDiscord();
+  return <Menu>
+    {
+      discord.isLoggedIn
+        ? <MenuItem onClick={discord.logout}>Log out of Discord</MenuItem>
+        : <MenuItem onClick={discord.login}>
+            <Typography>Log in to Discord</Typography>
+          </MenuItem>
+    }
+  </Menu>
+}
+```
+### Available actions
+
+#### login
+Initiate the login flow.
+
+#### logout
+Log the user out in this app. This will reset the state as well.
+
+### Available state
+
+#### avatar
+String - A url that points to an avatar of the user. For example:
+```js
+'https://cdn.discordapp.com/avatars/8574465478869584/7437496069584a98685e96968.png?size=32'
+```
+
+#### isLoggedIn
+Boolean - true if the user is logged in, otherwise false.
+
+#### roles
+Dictionary - A map of guild to array of roles that a uer has. For example:
+```js
+{
+  '12325234234234234': ['324234232523412341241', '23423512342345234123']
+}
+```
+
+#### uid
+String - The user id in discord. For example:
+```js
+'235436523432652'
+```
+
+#### username
+String - The username for the user. For example:
+```js
+'ftreesmilo'
+```
+
---
+++ index.js
@@ -0,0 +1,166 @@
+import React, { useState, useEffect, useContext, createContext, useCallback } from 'react';
+import { Promise } from 'bluebird';
+import challenge from 'pkce-challenge';
+import moment from 'moment';
+import useInterval from 'use-interval';
+import debug from 'debug';
+
+const log = debug('discord:log');
+const error = debug('discord:error');
+error.log = console.error.bind(console);
+const info = debug('discord:info');
+info.log = console.info.bind(console);
+
+const login = ({client_id, redirect_uri, scopes}) => {
+  const { code_challenge, code_verifier } = challenge(128);
+  navigator.serviceWorker.controller.postMessage({ type: 'oauth', code_verifier });
+  const url = new URL('https://discord.com/api/oauth2/authorize');
+  url.search = new URLSearchParams({
+    client_id,
+    code_challenge,
+    code_challenge_method: 'S256',
+    redirect_uri,
+    response_type: 'code',
+    scope: scopes.join(' '),
+  });
+  window.open(url.toString(), '_blank', 'popup=1');
+};
+
+const expired = (at) => moment(at).isBefore(moment());
+
+/**
+ * @param {[string]} guilds
+ */
+const discord = ({guilds, ...opts}) => {
+  const [tokens, setTokens] = useState(JSON.parse(localStorage.getItem('discord:token') || '{}'));
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [errors, setErrors] = useState([]);
+  const [last, setLast] = useState(localStorage.getItem('discord:last') || '');
+
+  const rolesState = useState(JSON.parse(localStorage.getItem('discord:roles') || '{}'));
+  /** @type {{[server: string]: [string]}} */
+  const roles = rolesState[0];
+  const setRoles = rolesState[1];
+
+  const userState = useState(JSON.parse(localStorage.getItem('discord:user') || '{}'));
+  /** @type {{ avatar: string, username: string, id: string }} */
+  const user = userState[0];
+  const setUser = userState[1];
+
+  useEffect(() => {
+    const { access_token, refresh_token, expires_at } = tokens;
+    if (!access_token || !refresh_token || !expires_at) {
+      setIsLoggedIn(false);
+      setUser({});
+      setRoles({});
+      return;
+    }
+
+    if (expired(expires_at)) {
+      setTokens({});
+    } else {
+      setIsLoggedIn(true);
+    }
+  }, [tokens]);
+
+  const handler = (event) => {
+    if (event.data.type !== 'oauth') return;
+    const { data } = event.data;
+    data.expires_at = moment().add(data.expires_in, 'seconds').toISOString();
+    setTokens(data);
+  };
+  useEffect(() => {
+    navigator.serviceWorker.addEventListener('message', handler);
+    return () => navigator.serviceWorker.removeEventListener('message', handler);
+  }, []);
+
+  const updateRoles = useCallback(() => {
+    if (!isLoggedIn) return;
+    if (user.avatar && user.id && user.username) {
+      if (last && moment(last).add(30, 'minutes').isAfter(moment())) return;
+    }
+
+    const options = { headers: { authorization: `Bearer ${tokens.access_token}` } };
+
+    Promise.props(
+      Object.fromEntries(guilds.map(guild => [
+        guild,
+        fetch(`https://discord.com/api/users/@me/guilds/${guild}/member`, options)
+          .then(r => r.json()),
+      ]))
+    )
+    .then(rsp => {
+      log('raw role resp: %O', rsp);
+      const u = Object.values(rsp)
+        .map(val => val.user)
+        .find((val) => Boolean(val));
+
+      u && setUser({
+        avatar: `https://cdn.discordapp.com/avatars/${u.id}/${u.avatar}.png?size=32`,
+        id: u.id,
+        username: u.username,
+      });
+      return rsp;
+    })
+    .then(rsp => Object.entries(rsp).reduce((prev, [key, val]) => {
+      prev[key] = val.roles;
+      return prev;
+    }, {}))
+    .tap(o => log('roles: %O', o))
+    .then(setRoles)
+    .then(() => setLast(moment().toISOString()))
+    .tapCatch(error)
+    .tapCatch(e => setErrors(...errors, e))
+    .catch(() => setTokens({}));
+  }, [isLoggedIn, tokens, user, errors, last]);
+
+  useEffect(updateRoles, [user, tokens, isLoggedIn]);
+  useInterval(updateRoles, 60 * 60 * 1000); // refresh every hour
+
+  useEffect(() => localStorage.setItem('discord:token', JSON.stringify(tokens)), [tokens]);
+  useEffect(() => localStorage.setItem('discord:user', JSON.stringify(user)), [user]);
+  useEffect(() => localStorage.setItem('discord:roles', JSON.stringify(roles)), [roles]);
+  useEffect(() => localStorage.setItem('discord:last', last), [last]);
+
+  const clearErrors = () => setErrors([]);
+  return {
+    errors,
+    clearErrors,
+    roles,
+    uid: user.id || '',
+    username: user.username || '',
+    avatar: user.avatar || '',
+    isLoggedIn,
+    login: () => login(opts),
+    logout: () => setTokens({}),
+  };
+};
+
+const ctx = createContext(null);
+export const useDiscord = () => useContext(ctx);
+
+/**
+ *
+ * @typedef {Object} DiscordProviderOptions
+ * @property {string} client_id
+ * @property {string} [redirect_uri]
+ * @property {[string]} [scopes]
+ * @property {[string]} guilds
+ */
+/** @param {DiscordProviderOptions} options */
+export const DiscordProvider = ({
+  children,
+  client_id,
+  redirect_uri = `${location.origin}/discord`,
+  scopes = ['identify', 'guilds.members.read'],
+  guilds = [],
+}) => {
+  if (!guilds?.length) throw new Error('You must specify at least 1 guild in guilds param DiscordProvider.');
+
+  const { Provider } = ctx;
+  return (
+    <Provider value={discord({ client_id, redirect_uri, scopes, guilds })}>
+      {children}
+    </Provider>
+  );
+};
